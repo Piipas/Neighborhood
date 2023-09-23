8b8f14b3fd4ebfdf7260d462ededb20496e22aa4
Pipas <ismailpipas@gmail.com>
2023-09-23 20:47:15 +0100

    Status Update Function Added

--- src/components/Calendars.jsx
+++ src/components/Calendars.jsx
@@ -1,27 +1,59 @@
 import React, { Component } from "react";
 import CalendarTable from './Calendar';
-import ap from '../json/AppartmentsContributions.json';
+import years from '../json/AppartmentsContributions.json';
 
 export default class CalendarTables extends Component {
   constructor() {
     super();
     this.state = {
       months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-      ap,
+      years,
       loading: true
     }
   }
 
+  updateMonthStatus = (appartment, year, month) => {
+    const duplicatedYears = this.state.years;
+    const originalMonthStatus = duplicatedYears[year][appartment-1].months[month];
+    duplicatedYears[year][appartment-1].months[month] = !Boolean(originalMonthStatus);
+    this.setState({years: duplicatedYears})
+  }
+
+  setMonthStatusNull = (appartment, year, month) => {
+    const duplicatedYears = this.state.years;
+    duplicatedYears[year][appartment-1].months[month] = null;
+    this.setState({years: duplicatedYears})
+  }
+
+  componentDidMount() {
+    this.setState({loading: false});
+  }
+
   render() {
-    const { months: m, ap } = this.state;
-    return (
-      <>
-        {Object.keys(ap.years).sort().reverse().map(year => {
-          return (
-            <CalendarTable key={year} year={ap.years[year]} months={m} y={year} />
-          )
-        })}
-      </>
-    );
+    const { months: m, years, loading } = this.state;
+    if (!loading) {
+      return (
+        <div className="calendar rounded-3 table-responsive">
+          <table className="table table-striped table-hover table-dark mb-0 text-center">
+            <thead>
+              <tr className="position-relative">
+                <th className="align-middle position-sticky start-0 z-2" scope="col">Year</th>
+                <th className="align-middle" scope="col">Number</th>
+                {this.state.months.map((month, index) => <th className="align-middle" key={index} scope="col">{month}</th> )}
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              {Object.keys(years).sort().reverse().map(year => {
+                return (
+                  <CalendarTable key={year} year={years[year]} months={m} y={year} updateMonthStatus={this.updateMonthStatus} setMonthStatusNull={this.setMonthStatusNull}/>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else { 
+      return <div className="calendar align-items-center justify-content-center d-flex"><i class="fa-solid fa-building fa-beat-fade"></i></div>
+    }
   }
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,23 +16,20 @@ export default class Navbar extends Component {
           icon: 'far fa-calendar fa-lg',
           page: 'calendar'
         }, {
-          id: 3,
-          name: 'Manage',
-          icon: 'fas fa-cog fa-lg',
-          page: 'manage'
-        }, {
           id: 4,
           name: 'Log in',
           icon: 'fas fa-right-to-bracket fa-lg',
           page: 'login'
         }
-      ]
+      ],
+      moneyBox: 2050
     }
   }
 
   render() {
     return (
       <div className="_navbar rounded-4 d-flex justify-content-evenly align-items-center bg-dark">
+        <span className="moneyBox bg-black pt-2 pb-2 px-3 rounded-3 align-items-center d-flex"><i className="fas fa-wallet fa-xl"></i><span className="ps-3 fs-5 fw-bold">{this.state.moneyBox} MAD</span></span>
         {this.state.links.map(link => { 
           return <a href={link.page} title={link.name} key={link.id} className="fa-lg text-light"><i className={link.icon}></i></a>
         })}
